55c2871b1d49c86b9de8d2b83e293e709096672f
Jazz Peh <jazzpehz@gmail.com>
2020-05-21 19:33:41 +0800

    Custom build for jekyll

--- _js/main/csv.js
+++
@@ -1,7 +0,0 @@
-import CsvToTable from '../modules/csv-to-table';
-
-const csv = () => {
-    [...document.querySelectorAll('code.language-csv')].forEach(e => new CsvToTable(e));
-};
-
-export default csv;
